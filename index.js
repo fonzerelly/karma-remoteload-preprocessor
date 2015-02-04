a8b4df5b67db322ac4a98e4cb42eea08eef0992b
fonzerelly <ChristianHoerauf@web.de>
2015-02-04 22:34:28 +0100

    index.js only routes to ui.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 console.log("plugin recognized");
-var createRemoteLoader = function (loggerFactory, conf) {
-  console.log(conf);
-  //var logger = loggerFactory.create("preprocessor:remoteload");
-
-  return function (content, file, done) {
-    console.log(file);
-    done(content.replace("doof", "intelligent"));
-  };
-};
-createRemoteLoader.$inject = ['logger', 'config.remoteloadPreprocessor'];
+var remoteloadUI = require("./ui.js");
+// var createRemoteLoader = function (loggerFactory, conf) {
+//   var logger = loggerFactory.create("preprocessor:remoteload");
+//   var keys = function (obj) {
+//     for(var key in obj) {
+//       console.log(key);
+//     }
+//   };
+//   keys("factory", loggerFactory);
+//   keys("logger", logger);
+//   console.log("rainbow", logger.rainbow);
+//   console.log("trace", logger.trace.toString());
+//
+//   return function (content, file, done) {
+//     console.log(file);
+//     done(content.replace("doof", "intelligent"));
+//   };
+// };
+// createRemoteLoader.$inject = ['logger', 'config.remoteloadPreprocessor'];
+//
+remoteloadUI.createRemoteLoader.$inject = ['config.remoteloadPreprocessor'];
 
 module.exports = {
-  'preprocessor:remoteload': [ 'factory', createRemoteLoader]
+  'preprocessor:remoteload': [ 'factory', remoteloadUI.createRemoteLoader]
 };
